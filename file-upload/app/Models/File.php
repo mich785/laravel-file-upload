b242f06c1f3442e2786dfe56fd7ec65441d3a2cb
mich785 <nderimichelle@gmail.com>
2024-08-22 12:50:35 +0300

    "Added fillable fields to File model"

--- file-upload/app/Models/File.php
+++ file-upload/app/Models/File.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'mime_type', 
+        'file_data'
+    ];
 }
